496372ebc8f4971778bf9ad788752f5b0a0535b9
CryptoHunter299 <96936412+CryptoHunter299@users.noreply.github.com>
2022-01-07 16:00:44 +0700

    Create freethai.php

---
+++ freethai.php
@@ -0,0 +1,39 @@
+<?php
+/*
+Hayoo mau ngapain
+*/
+
+echo "\n\e[0;31m]
+         .---.        .-----------
+        /     \  __  /    ------
+       / /     \(••)/    -----
+      //////   * \/ *   ---
+     //// / // :    : ---
+    // /   /  /*    *--
+   //          //..\\
++=============UU====UU=======================+
++              *//||\\*                      +
++                ****                        +
++ coded by : CryptoHunter299 | Manusia              +
++ Team     : LNX#CREW   | S.T.C | BUFT         +
++ GitHub   : https://github.com/CryptoHunter299  +
++============================================+
+                              Version 1.1.1\e[0m \r\n";
+echo "\n\e[0;36m]
+--==::{Thai Gratis}::==--";                              
+echo "\n\e[0;34m]Contoh : 0234xxxxxx";
+echo "\n\e[0;33m]
+Masukkan Thai ID Anda: \e[0m \r\n";
+$no = trim(fgets(STDIN, 1024));
+echo "\n\e[0;33m]
+Jumlah Thai Yang Anda Mau: \e[0m \r\n";
+$ju = trim(fgets(STDIN, 1024));
+echo "\n\e[0;32m]
+$no , Ok, Otw Kirim Thai nya Sebesar , $ju\e[0m \r\n";
+echo "\n\e[0;34m]
+Harap Wait 200 Abad Ya Gan\e[0m \r\n";
+echo "\n\e[0;34m]
+Kalo Gak kekirim, anda kurang beruntung\e[0m \r\n";
+echo "\n\e[0;34m]
+Btw Follow GitHub Gw Yak\e[0m \r\n";
+?>
