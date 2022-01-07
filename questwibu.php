5b97de2a565a58809077b1897420f6d94838e06c
CryptoHunter299 <96936412+CryptoHunter299@users.noreply.github.com>
2022-01-07 15:25:22 +0700

    Create questwibu.php

---
+++ questwibu.php
@@ -0,0 +1,39 @@
+<?php
+/*
+Hayoo mau ngapain
+*/
+
+echo "\n\e[0;31m]
+         .---.        .-----------
+        /     \  __  /    ------
+       / /     \(••)/    -----
+      //////   * \/ *   ---
+     //// / // :    : ---
+    // /   /  /*    *--
+   //          //..\\
++=============UU====UU=======================+
++              *//||\\*                      +
++                ****                        +
++ coded by : CryptoHunter299 | Manusia              +
++ Team     : LNX#CREW   | S.T.C | BUFT         +
++ GitHub   : https://github.com/CryptoHunter299  +
++============================================+
+                              Version 1.1.1\e[0m \r\n";
+echo "\n\e[0;36m]
+--==::{Pertanyaan Buat Wibu}::==--";                              
+echo "\n\e[0;34m]Contoh : Boku No Pico";
+echo "\n\e[0;33m]
+Anime Apa Yg Lu Sukai: \e[0m \r\n";
+$no = trim(fgets(STDIN, 1024));
+echo "\n\e[0;33m]
+Apakah Lu Wibu Sangad Elit?: \e[0m \r\n";
+$ju = trim(fgets(STDIN, 1024));
+echo "\n\e[0;32m]
+$no , Okey Tq Udh Ngisi Survey Ini , $ju\e[0m \r\n";
+echo "\n\e[0;34m]
+Lu Gabut y?:v\e[0m \r\n";
+echo "\n\e[0;34m]
+Eh, Lu Masih Gabut?:v\e[0m \r\n";
+echo "\n\e[0;34m]
+Btw Follow GitHub Gw Yak\e[0m \r\n";
+?>
